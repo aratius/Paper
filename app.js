5059b24031a4587f7a83fb924d1a691d1f911e58
Arata Matsumoto <arata1129matsu@icloud.com>
2022-11-07 17:30:49 +0900

    ...

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const Random = require("./lib/random")
+const BarCode = require("./lib/barCode")
 
 const init = () => {
-  const random = new Random()
-  random.start()
+  // const random = new Random()
+  // random.start()
+  const barCode = new BarCode()
+  barCode.start()
 }
 
 init()
